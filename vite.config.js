1756a423566ee0d0dbaece4d1e238fb4409f3389
Alexey Masasin <alexeymasasin@gmail.com>
2025-05-18 20:04:05 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/happy-birthday-anechka',
+	base: '/happy-birthday-anechka/',
 	plugins: [react()],
 });
